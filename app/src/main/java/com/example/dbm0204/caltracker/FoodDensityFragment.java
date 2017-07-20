6f46ef52d15b9dcf41f28de2d8282eb124c1554d
bmathew2014 <bmathew2014@my.fit.edu>
2017-07-20 14:24:47 +0530

    Alert Dilaog and Error Dialog Added

---
+++ app/src/main/java/com/example/dbm0204/caltracker/FoodDensityFragment.java
@@ -0,0 +1,11 @@
+package com.example.dbm0204.caltracker;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by dbm0204 on 7/20/17.
+ */
+
+public class FoodDensityFragment extends DialogFragment {
+    public FoodItem
+}

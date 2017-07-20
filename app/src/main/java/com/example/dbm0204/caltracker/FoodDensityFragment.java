5a96995a18a5b0ea4717f77ab34463844a6436f1
bmathew2014 <bmathew2014@my.fit.edu>
2017-07-20 14:30:28 +0530

    Working on FoodDensityFragment

--- app/src/main/java/com/example/dbm0204/caltracker/FoodDensityFragment.java
+++ app/src/main/java/com/example/dbm0204/caltracker/FoodDensityFragment.java
@@ -1,11 +1,30 @@
 package com.example.dbm0204.caltracker;
 
 import android.app.DialogFragment;
+import android.view.View;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by dbm0204 on 7/20/17.
  */
 
 public class FoodDensityFragment extends DialogFragment {
-    public FoodItem
+    public FoodItem food;
+    private View view;
+    private Map<String, Double> matches;
+    private List<String> StringList;
+
+    public static FoodDensityFragment newInstance(FoodItem food, Map<String,Double> matches){
+        FoodDensityFragment fragment = new FoodDensityFragment();
+        fragment.food=food;
+        fragment.matches=matches;
+        return fragment;
+    }
+    public interface FoodDensityDialogListener{
+        public void onDensityDialogPositiveClick(DialogFragment dialog,String name, Double value);
+        public void onDensityDiaLogNeutralClick(DialogFragment dialog);
+    }
+
 }
